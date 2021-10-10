bd3875d0d5d3bfd606cc356218e6a60c7da6a955
AllahDalla <alromariodavis@gmail.com>
2021-10-09 22:58:28 -0400

    Added X and O to boxes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,23 @@ window.onload = function() {
   
    for(let x = 0; x < gameboard.length; x++){
      gameboard[x].setAttribute('class','square');
-   };
+     
+     }
+     var tracker = [];
+    for(let i = 0; i < gameboard.length; i++){
+        gameboard[i].addEventListener("click", function(e){
+            if (tracker.length == 0 || (tracker.length % 2) == 0){
+                e.target.textContent = "O";
+                e.target.classList.add('square', 'O');
+                console.log("I have been clicked for O");
+                tracker.push("O");
+            }else if ((tracker.length % 2) > 0){
+                e.target.textContent = "X";
+                e.target.classList.add('square', 'X');
+                console.log("I have been clicked for X");
+                tracker.push("X");
+            }
+        });  
+    };
+};   
   
-  }
